233753cad8b3f437c68b92ef44fd6f3dd6d93cfd
apmechev <apmechev@gmail.com>
2018-02-19 10:46:47 +0100

    Made the package installable preparing for the pypi upload

--- github_clones_badge/get_github_clones.py
+++ github_clones_badge/get_github_clones.py
@@ -7,6 +7,9 @@ import os
 from urllib import FancyURLopener
 import crontab
 
+global DATADIR
+DATADIR = os.path.dirname(os.path.realpath(__file__))+"/data/"
+
 class git_creds(object):
     def __init__(self,username=None, password=None, gitfile='git_creds'):
         self.username=username
@@ -32,18 +35,18 @@ class git_clones_counter(object):
         if not password:
             self.password=git_creds( gitfile='git_creds').password
         self.repo=reponame
-        if not os.path.exists('data/'+self.repo+'_clones.pkl'):
+        if not os.path.exists(DATADIR+self.repo+'_clones.pkl'):
             self.initialize_repo()
         self.clones={}
         self.get_previous_clones()
 
     def initialize_repo(self):
-        if os.path.exists('data/'+self.repo+'_clones.pkl'):
-            os.remove('data/'+self.repo+'_clones.pkl')
-        pickle.dump({},open('data/'+self.repo+'_clones.pkl','w'))
+        if os.path.exists(DATADIR+self.repo+'_clones.pkl'):
+            os.remove(DATADIR+self.repo+'_clones.pkl')
+        pickle.dump({},open(DATADIR+self.repo+'_clones.pkl','w'))
 
     def get_previous_clones(self):
-        self.clones=pickle.load(open('data/'+self.repo+'_clones.pkl','rb'))
+        self.clones=pickle.load(open(DATADIR+self.repo+'_clones.pkl','rb'))
 
     def get_git_clones_json(self,username=None):
         if not username:
@@ -64,8 +67,8 @@ class git_clones_counter(object):
             return
         for item in json_output.json()['clones']:
             self.clones[item['timestamp']]=item['count']
-        if len(self.clones.keys()) >= len(pickle.load(open('data/'+self.repo+'_clones.pkl','rb'))):
-            pickle.dump(self.clones,open('data/'+self.repo+'_clones.pkl','wb'))
+        if len(self.clones.keys()) >= len(pickle.load(open(DATADIR+self.repo+'_clones.pkl','rb'))):
+            pickle.dump(self.clones,open(DATADIR+self.repo+'_clones.pkl','wb'))
 
     def count_clones(self):
         s=0
@@ -74,34 +77,42 @@ class git_clones_counter(object):
         self.count=str(s)
         if s==0:
             self.color='red'
+            return
         if s<10:
             self.color="orange"
+            return
         if s<100:
             self.color="yellow"
+            return
         if s<1000:
             self.color="yellowgreen"
+            return
         elif s<1000000:
             self.count=str(s/1000)+"K+"
             self.color="green"
+            return
         elif s<1000000000:
             self.count=str(s/1000)+"M+"
             self.color="brightgreen"
  
-import pdb
+
 class badge_creator(object):
-    def __init__(self,repo,username=None,password=None):
+    def __init__(self,repo,username=None,password=None,repouser=None):
         self.clones=git_clones_counter(repo,username,password)
-        new_clones=self.clones.get_git_clones_json()
-        print(str(len(new_clones.json().keys()))+ " Number of new entries")
+        if repouser:
+            new_clones=self.clones.get_git_clones_json(username=repouser)
+        else:
+            new_clones=self.clones.get_git_clones_json()
+        print(str(len(new_clones.json()['clones']))+ " Number of new entries")
         self.clones.parse_clones_json(new_clones)
-        self.clones.count_clones()
+        self.clones.count_clones() 
         self.count=self.clones.count
         self.color=self.clones.color
 
     def download_badge(self,dest_path="/var/www/html/apmechev.com/public_html/img/git_repos/"):
         mopen = MyOpener()
-        mopen.retrieve("https://img.shields.io/badge/clones-"+self.count+"-"+self.color+".svg",dest_path+self.clones.repo+"_clones.svg")
-
+        mopen.retrieve("https://img.shields.io/badge/clones-"+self.count+"-"+self.color+".svg",dest_path+"/"+self.clones.repo+"_clones.svg")
+        print("done")
 
 user='apmechev'
 
---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'github_clones_badge',
+  packages = ['github_clones_badge'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'A small package to create badges that count github clones',
+  author = 'Alexandar Mechev',
+  author_email = 'apmechev@gmail.com',
+  url = 'https://github.com/apmechev/github_clones_badge', 
+  download_url = 'https://github.com/apmechev/github_clones_badge/archive/0.1.0.tar.gz', 
+  keywords = ['github', 'github_badges' ], # arbitrary keywords
+  classifiers = ['Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.2',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',],
+)
+
