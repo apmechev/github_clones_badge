15fa5ea5f5a974b3802b2a8c14b286427a738363
apmechev <apmechev@gmail.com>
2018-02-22 11:14:16 +0100

    Starting travis integration

---
+++ github_clones_badge/tests/test_badge_creator.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+
+
+
+class git_clones_counterTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def init_test(self):
+        from github_clones_badge import get_github_clones as ghc
+        gc=git_clones_counter(reponame, username=os.environ['GITUSR'], password=os.environ['GITPASS'])
+
+#       self.assertTrue(sl.LTA_location==None)
+
